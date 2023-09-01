33825171a87daf89e55fa63ab74201d7134c685c
kiranp1984 <143506509+kiranp1984@users.noreply.github.com>
2023-09-01 13:00:10 +0530

    Delete customer-loan-service directory
    removing duplicate folder

--- customer-loan-service/src/main/java/com/ing/loan/exception/UsernameNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ing.loan.exception;
-
-public class UsernameNotFoundException extends RuntimeException{
-    public UsernameNotFoundException(String message) {
-        super(message);
-    }
-}
