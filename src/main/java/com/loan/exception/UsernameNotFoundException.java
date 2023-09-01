8c548fcf1223b6e4e8b153c8099add90f9276464
kiranp1984 <143506509+kiranp1984@users.noreply.github.com>
2023-09-01 20:16:00 +0530

    Delete src directory

--- src/main/java/com/loan/exception/UsernameNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.loan.exception;
-
-public class UsernameNotFoundException extends RuntimeException{
-    public UsernameNotFoundException(String message) {
-        super(message);
-    }
-}
